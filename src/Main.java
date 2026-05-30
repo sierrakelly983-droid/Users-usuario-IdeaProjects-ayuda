67d6c5420a2a924e266792f35bb763af7bbeb847
kelly sierra <ksierrap@unicartagena.edu.co>
2026-05-30 11:08:56 -0500

    mejore el programa

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    IO.println("Tabla de multiplicar del 5");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    for (int i = 1; i <= 10; i++) {
+        IO.println("5 x " + i + " = " + (5 * i));
     }
 }
